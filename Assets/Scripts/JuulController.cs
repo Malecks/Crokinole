be74cbf1be49795a5d6a60770055b9f82eac40f9
Malecks <alex@alexmathers.com>
2020-07-20 13:16:39 -0400

    Update JUUL bounce

--- Assets/Scripts/JuulController.cs
+++ Assets/Scripts/JuulController.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class JuulController : MonoBehaviour
 {
-    private float maxTorque = 10.0f;
-    private float minJump = 4.0f;
-    private float maxJump = 6.5f;
+
 
     private void OnMouseDown()
     {
@@ -21,6 +19,8 @@ public class JuulController : MonoBehaviour
 
     Vector3 RandomTorque()
     {
+        float maxTorque = 10.0f;
+
         return new Vector3(
             Random.Range(-maxTorque, maxTorque),
             Random.Range(-maxTorque, maxTorque),
@@ -30,6 +30,9 @@ public class JuulController : MonoBehaviour
 
     Vector3 RandomForce()
     {
+        float minJump = 4.0f;
+        float maxJump = 6.5f;
+
         return Vector3.up * Random.Range(minJump, maxJump);
     }
 }
